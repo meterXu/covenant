6f0f41adb6c9962b97caffa4f90f11e07591144c
isaac <xuhg@dpark.com.cn>
2021-01-14 16:38:37 +0800

    ....

--- pages/index.js
+++ pages/index.js
@@ -49,14 +49,13 @@ class Home  extends React.Component{
                       </Menu>
                   </Sider>
                   <Layout className={styles['site-layout']}>
-                      <Header className={styles['site-layout-background']} style={{ padding: 0 }}>
+                      <Header style={{ padding: 0 }}>
                           {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
                               className: styles.trigger,
                               onClick: this.toggle,
                           })}
                       </Header>
                       <Content
-                          className={styles['site-layout-background']}
                           style={{
                               margin: '24px 16px',
                               padding: 24,
