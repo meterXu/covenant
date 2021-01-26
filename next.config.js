48d996a5a31c9e2c9b06e09f77294845ce6d5548
isaac <xuhg@dpark.com.cn>
2021-01-26 12:12:20 +0800

    ....

--- next.config.js
+++ next.config.js
@@ -1,29 +1,17 @@
-const fs = require("fs");
+const path = require('path');
 const withAntdLess = require('next-plugin-antd-less');
-const AntDesignThemePlugin = require('antd-theme-webpack-plugin');
+const { getThemeVariables } = require('antd/dist/theme');
 
-const options = {
-    antDir: path.join(__dirname, './node_modules/antd'),
-    stylesDir: path.join(__dirname, './pages'),
-    themeVariables: ['@primary-color'],
-    indexFileName: 'index.html',
-    generateOnce: false,
-    lessUrl: "https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js",
-    publicPath: "",
-    customColorRegexArray: [], // An array of regex codes to match your custom color variable values so that code can identify that it's a valid color. Make sure your regex does not adds false positives.
-}
-
-const themePlugin = new AntDesignThemePlugin(options);
 
 
 module.exports = withAntdLess({
-    modifyVars: {
+    modifyVars: getThemeVariables({
         dark:true
-    },
+    }),
+    lessVarsFilePath:path.join('./pages/styles/vars.less'),
     cssLoaderOptions: {
     },
     webpack(config) {
-
         return config;
     },
 });
