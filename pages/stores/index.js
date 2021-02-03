21bbc291c491d37b4a0a31de08f0afb20993c9ac
IsaacXu <xuhonggang@hotmail.com>
2021-02-04 00:02:58 +0800

    ...

--- pages/stores/index.js
+++ pages/stores/index.js
@@ -3,9 +3,13 @@ import {action, computed, makeObservable, observable, runInAction} from "mobx";
 export default class Store {
     constructor() {
         makeObservable(this)
+        if(localStorage.getItem('theme')){
+            this.theme  = localStorage.getItem('theme')
+        }
     }
     @observable theme = 'dark'
     @action setTheme = (theme)=>{
         this.theme = theme
+        localStorage.setItem('theme',theme)
     }
 }
