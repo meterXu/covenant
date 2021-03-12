e443a3471f65066d389e88fdab5c56c8af878d43
isaac <xuhg@dpark.com.cn>
2021-03-12 16:44:49 +0800

    ....

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const Koa = require('koa')
-const charset = require('koa-charset');
 const cors = require('koa2-cors');
 const next = require('next')
 const router = require('./router/index')
@@ -10,18 +9,15 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
     const server = new Koa()
-
     router.all('(.*)', async (ctx) => {
         await handle(ctx.req, ctx.res)
         ctx.respond = false
     })
-
     server.use(async (ctx, next) => {
         ctx.res.statusCode = 200
         await next()
     })
-    // server.use(cors());
-    // server.use(charset)
+    server.use(cors());
     server.use(router.routes())
     server.listen(port, () => {
         console.log(`> Ready on http://localhost:${port}`)
