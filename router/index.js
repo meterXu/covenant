f162de9d7a6fc2dabfcc559350337d293451b25e
isaac <xuhg@dpark.com.cn>
2021-03-16 16:09:42 +0800

    ....

--- router/index.js
+++ router/index.js
@@ -5,6 +5,6 @@ const dic = require('./dic')
 const response =  require('./response')
 router.use('(/mocky.*)', mocky.routes(), mocky.allowedMethods())
 router.use('/collection', collection.routes(), collection.allowedMethods())
-router.use('/dic', dic.routes(), mocky.allowedMethods())
+router.use('/dic', dic.routes(), dic.allowedMethods())
 router.use('/response', response.routes(), response.allowedMethods())
 module.exports = router;
