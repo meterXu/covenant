e16296c86b036f7198834d453c6dbb0b929e96f0
isaac <xuhg@dpark.com.cn>
2021-03-18 14:34:10 +0800

    ....

--- pages/components/Icon.js
+++ pages/components/Icon.js
@@ -7,8 +7,8 @@ const DarkSvg = () => (
     <svg width="1em" height="1em" viewBox="0 0 24 24" aria-hidden="true" fill="currentColor"><path d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"></path></svg>
   )
 
-const DarkIcon = props => <Icon component={DarkSvg} {...props} />;
+export const DarkIcon = props => <Icon component={DarkSvg} {...props} />;
 
-const LightIcon = props => <Icon component={LightSvg} {...props} />;
+export const LightIcon = props => <Icon component={LightSvg} {...props} />;
 
-export {DarkIcon,LightIcon}
+export default DarkIcon
